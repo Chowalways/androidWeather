6c1b9292221beba4ebb60605a212a544df9feacc
Chow <travisckelly@yahoo.com>
2018-06-06 23:02:33 +0800

    add temp Still to make pretty

---
+++ app/src/main/java/com/example/chow/weatherv2/Typhoon.java
@@ -0,0 +1,14 @@
+package com.example.chow.weatherv2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Typhoon extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_typhoon_warnings);
+
+    }
+}
