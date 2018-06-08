b6a63307ff1cd333b047fbcb4b6acd38ca575c69
xilanneko <xilanneko@gmail.com>
2018-06-08 14:27:59 +0800

    預計抓的資料

--- app/src/main/java/com/example/chow/weatherv2/EarthQuakeWarnings.java
+++ app/src/main/java/com/example/chow/weatherv2/EarthQuakeWarnings.java
@@ -2,15 +2,18 @@ package com.example.chow.weatherv2;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
 //uninportnt
 
 public class EarthQuakeWarnings extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_earthquake);
+        setContentView(R.layout.activity_main);
 
     }
+
 
 }
---
+++ app/src/main/java/com/example/chow/weatherv2/earthQuakeList.java
@@ -0,0 +1,41 @@
+package com.example.chow.weatherv2;
+
+/**
+ * Created by user on 2018/5/31.
+ */
+public class earthQuakeList {
+
+    //private Bitmap imgUrl;
+    private String extent;
+    private String locale;
+    private String depth;
+
+    public String getExtent() {
+        return extent;
+    }
+
+    public void setExtent(String extent) {
+        this.extent = extent;
+    }
+
+    public String getLocale() {
+        return locale;
+    }
+
+    public void setLocale(String locale) {
+        this.locale = locale;
+    }
+
+    public String getDepth() {
+        return depth;
+    }
+
+    public void setDepth(String depth) {
+        this.depth = depth;
+    }
+
+
+}
+
+
+
